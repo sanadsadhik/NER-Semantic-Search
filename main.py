7377bddd6090bddd6baa669ae309fdfa1c38c811
sanadsadhik <56155278+sanadsadhik@users.noreply.github.com>
2024-12-09 12:55:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
+from sentence_transformers import SentenceTransformer
+from pinecone import Pinecone, ServerlessSpec
+from datasets import load_dataset
+import pandas as pd
+
 import torch
 
 from config import PINECONE_API_KEY
@@ -17,5 +22,32 @@ nlp = pipeline(
     aggregation_strategy='max',
     device='cpu'
 )
+
+retriever = SentenceTransformer("flax-sentence-embeddings/all_datasets_v3_mpnet-base")
+
+api_key = PINECONE_API_KEY
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+pc.create_index(
+    name="medium-data",
+    dimension=768,
+    metric="cosine",
+    spec=ServerlessSpec(    
+        cloud="aws",
+        region="us-east-1"
+    )
+)
+
+idx = pc.Index('medium-data')
+
+df = load_dataset(
+    "fabiochiu/medium-articles",
+    data_files="medium_articles.csv",
+    split="train"
+).to_pandas()
+
+df = df.iloc[0:1000]
+
+
 
 
